1df449817c656f4c5b86f886cb06e2f33f7d52b8
Nestle-afk <nastassya04@gmail.com>
2026-02-02 21:10:05 +0300

    fix: CI Pipeline (cassandra)

--- src/main/java/com/arca/rate/config/CassandraConfig.java
+++ src/main/java/com/arca/rate/config/CassandraConfig.java
@@ -1,12 +1,14 @@
 package com.arca.rate.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.repository.config.EnableReactiveCassandraRepositories;
 
 @Configuration
 @EnableReactiveCassandraRepositories(basePackages = "com.arca.rate.repository.cassandra")
+@Profile("!test")
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Override
